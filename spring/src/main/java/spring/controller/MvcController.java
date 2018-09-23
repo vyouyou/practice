8a83666159c2ad2e90a7b20b760b2d166c991cb6
youyou <vqishiyu1@163.com>
2018-09-23 16:03:30 +0800

    fix

--- spring/src/main/java/spring/controller/MvcController.java
+++ spring/src/main/java/spring/controller/MvcController.java
@@ -13,48 +13,51 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 import javax.servlet.ServletRequest;
 import java.io.IOException;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 @Log
 @Controller
 public class MvcController {
     @GetMapping("/to-baidu")
-    public ModelAndView toBaidu(){
+    public ModelAndView toBaidu() {
         ModelAndView modelAndView = new ModelAndView("redirect:https://www.baidu.com");
         return modelAndView;
     }
 
     @GetMapping("/method")
     @ResponseBody
-    public String method(WebRequest webRequest, ServletRequest request){
-        log.info("i am host:"+webRequest.getHeader("host"));
+    public String method(WebRequest webRequest, ServletRequest request) {
+        log.info("i am host:" + webRequest.getHeader("host"));
         log.info("i am request characterEncoding" + request.getCharacterEncoding());
         return "method";
     }
 
     @GetMapping("/set-cookie")
-    public ResponseEntity<String> setCookie(){
+    public ResponseEntity<String> setCookie() {
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Set-Cookie","name=youyou");
-        ResponseEntity<String> entity = new ResponseEntity<>("i am  cookie",headers,HttpStatus.OK);
+        headers.set("Set-Cookie", "name=youyou");
+        ResponseEntity<String> entity = new ResponseEntity<>("i am  cookie", headers, HttpStatus.OK);
         return entity;
     }
 
     @GetMapping("/get-cookie")
     @ResponseBody
-    public String getCookie(@CookieValue("name")String name){
-        log.info("get cookie name "+name);
+    public String getCookie(@CookieValue("name") String name) {
+        log.info("get cookie name " + name);
         return "get-cookie";
     }
 
 
     /**
      * 只有当emitter complete的时候，才会返回
+     *
      * @return
      * @throws IOException
      */
@@ -63,7 +66,7 @@ public class MvcController {
         ResponseBodyEmitter emitter = new ResponseBodyEmitter();
         emitter.send("hello");
         emitter.send("hello again");
-        new Thread(()->{
+        new Thread(() -> {
             try {
                 Thread.sleep(2000);
                 emitter.complete();
@@ -77,26 +80,50 @@ public class MvcController {
     /**
      * 设置了跨域
      * {@link spring.mvcconfig.WebConfig}
+     *
      * @return1
      */
     @PostMapping("/cors")
     @ResponseBody
-    public String corsRequest(){
+    public String corsRequest() {
         return "i am cors";
     }
 
     @PostMapping("/xmlobject")
     @ResponseBody
-    public XmlObject getXmlObject(){
+    public XmlObject getXmlObject() {
         XmlObject xmlObject = new XmlObject();
         xmlObject.setName("heihei");
         xmlObject.setSex("nan");
         return xmlObject;
     }
 
+    @GetMapping("/async-request")
+    @ResponseBody
+    public DeferredResult<String> asyncRequest() {
+        DeferredResult<String> deferredResult = new DeferredResult<>();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            deferredResult.setResult("i am async result");
+        }).start();
+        return deferredResult;
+    }
+
+    @GetMapping("/async-callable")
+    @ResponseBody
+    public Callable<String> asyncCallable() {
+        return () -> "i am async callable";
+
+    }
+
     @Setter
     @Getter
-    class XmlObject{
+    class XmlObject {
         private String name;
 
         private String sex;
